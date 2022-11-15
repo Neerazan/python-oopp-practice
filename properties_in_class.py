80bf77312a187d6a4d65524b93c800c0d6b70a94
Neerajan-Dhakal <nirajandhakal634@gmail.com>
2022-11-15 19:02:51 +0545

    properties of classes

--- properties_in_class.py
+++ properties_in_class.py
@@ -12,4 +12,29 @@ print("ID = ", jack.ID)
 print("Salary = ", jack.salary)
 print("Department = ", jack.department)
 
+print(" ")
+print(" ")
+
 #creating property outside the class
+
+class Staff:
+    ID = None
+    salary = None
+    department = None
+
+#creating object
+selena = Staff()
+
+#assign values to properties of selena
+selena.ID = "NPI000098"
+selena.salary = 90000
+selena.department = "AI"
+
+# creating property outside the class
+selena.title = "senior engineer"
+
+#printing properties of selena
+print("ID = ", selena.ID)
+print("Salary = ", selena.salary)
+print("Department = ", selena.department)
+print("Title = ", selena.title)
