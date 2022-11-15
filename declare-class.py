f206aa79adde78eaa9296582e740dd437c027390
Neerajan-Dhakal <nirajandhakal634@gmail.com>
2022-11-15 18:35:11 +0545

    create first class and its object

---
+++ declare-class.py
@@ -0,0 +1,11 @@
+#python class name should be in PascalCase or in camelCase.
+class ClassName:
+    pass
+
+#create object of ClassName class"
+
+obj =ClassName()
+
+#printing obj will show the memory address at which this object is stroed.
+print(obj) 
+print("Hello world")
