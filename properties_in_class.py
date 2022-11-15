03bafc96c24e7594ed93466970f917f43602c955
Neerajan-Dhakal <nirajandhakal634@gmail.com>
2022-11-15 18:49:09 +0545

    creating new file for property

---
+++ properties_in_class.py
@@ -0,0 +1,15 @@
+#creating property inside the class
+class Emoployee:
+    ID = "NPI000099"
+    salary = 80000
+    department = "Compuer Engineer"
+
+#creating object
+jack = Emoployee()
+
+#printing the property of the jack
+print("ID = ", jack.ID)
+print("Salary = ", jack.salary)
+print("Department = ", jack.department)
+
+#creating property outside the class
