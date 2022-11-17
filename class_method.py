d147de24033935a7a5ea0679e9954db80cb352d4
Neerajan-Dhakal <nirajandhakal634@gmail.com>
2022-11-17 19:28:48 +0545

    class method

---
+++ class_method.py
@@ -0,0 +1,23 @@
+class player:
+    teamName = 'Barcelona'
+
+    # def __init__(self,name):
+    #     self.name = name
+
+    @classmethod
+    def getTeamName(cls): #use can use any other namine insted of self.
+        return cls.teamName
+
+print(player.getTeamName())
+
+
+# class Student:
+#     studentName = 'Shristi'
+#
+#     def __int__(self,name):
+#         self.name = name
+#
+#     def getStudentName(self):
+#         return self.studentName
+#
+# print(Student.getStudentName())
